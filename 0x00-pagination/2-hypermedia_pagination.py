64467cc9864b66d3f00f9e83ec9a27d6cda38210
math198512 <thoraichi@gmail.com>
2024-08-25 19:59:59 +0100

    2-hypermedia_pagination.py

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    takes two integer arguments page and page_size
+    returns a tuple containing a start index and an end index
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        hyper_dict = {}
+        returned_data = self.get_page(page, page_size) 
+        hyper_dict["page_size"] = page_size
+        hyper_dict["page"] = page
+        hyper_dict["data"] = returned_data
+
+        
+        return hyper_dict
