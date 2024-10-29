9245dfce0870a2fb29f160ba0f0644a1476d0bad
math198512 <thoraichi@gmail.com>
2024-10-29 23:38:05 +0100

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """put method"""
+        self.cache_data[key] = ite
