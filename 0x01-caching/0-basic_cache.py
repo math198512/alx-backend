5faa2ac2b28127c452c4c1f100d8ed97fb5c8145
math198512 <thoraichi@gmail.com>
2024-10-29 23:40:46 +0100

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,3 +10,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """put method"""
         self.cache_data[key] = item
+
+    def get(self, key):
+        """get method"""
+        return self.cache_data[key]
