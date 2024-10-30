30f9de2fb3b9f536435059eddfd71ea0e3eb16ec
math198512 <thoraichi@gmail.com>
2024-10-30 12:31:41 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,7 +15,17 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """put method"""
         if item is not None and key is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_discarded = self.key_indexes.pop(0)
+                del self.cache_data[item_discarded]
+                print("DISCARD:", item_discarded)
+
             self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """get method"""
