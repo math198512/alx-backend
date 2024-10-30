1a2c470bf66d52944849b312b2ffb1c4979d132d
math198512 <thoraichi@gmail.com>
2024-10-30 12:23:32 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" Basic dictionary """
+""" FIFO caching """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache class """
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.key_indexes = []
 
     def put(self, key, item):
         """put method"""
