6323312910e39013defcfd98c46aa834597cd3b4
math198512 <thoraichi@gmail.com>
2024-11-05 16:42:14 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=['GET'], strict_slashes=False)
 def index():
     """template that simply outputs Welcome to Holberton"""
     return render_template('0-index.html')
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     """ Main Function """
+#     app.run(host='0.0.0.0', port=5000)
