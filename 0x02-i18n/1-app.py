489289cc5d355bcf1b35e5a4a724dd75e6c77b43
math198512 <thoraichi@gmail.com>
2024-11-05 23:33:13 +0100

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
+import babel
 from flask import Flask, render_template
+from flask_babel import Babel
+
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def index():
     """template that simply outputs Welcome to Holberton"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
