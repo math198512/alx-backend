d3f80c90adb7e21a94b0f9f960b975bca9811e40
math198512 <thoraichi@gmail.com>
2024-11-05 16:28:42 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
+def index():
     """template that simply outputs Welcome to Holberton"""
-    return render_template('templates/0-index.html')
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
     """ Main Function """
