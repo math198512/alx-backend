459408607ab619cfdd78dc106f25da08f87bd457
math198512 <thoraichi@gmail.com>
2024-11-05 16:26:58 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     """template that simply outputs Welcome to Holberton"""
-    return render_template('0-index.html')
+    return render_template('templates/0-index.html')
 
 if __name__ == "__main__":
     """ Main Function """
