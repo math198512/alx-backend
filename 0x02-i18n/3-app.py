824cc7d0cff1ee9df0b7f544084fb5bd89a7bd58
math198512 <thoraichi@gmail.com>
2024-11-06 10:04:05 +0100

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+import babel
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages."""
+    return request.accept_languages.best_match(['fr', 'en'])
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def index():
+    """template that simply outputs Welcome to Holberton"""
+    return render_template('2-index.html')
